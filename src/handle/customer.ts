aae555d5e157d1d606f8bece8f42efb4dbe5628e
summer <x.xiaokang@gmail.com>
2024-01-10 22:49:51 +0800

    init

--- src/handle/customer.ts
+++ src/handle/customer.ts
@@ -1,41 +1,59 @@
 import { Customer, CustomerService } from "@medusajs/medusa";
 import { InjectedDependencies, EventBusResponse } from "../types";
+import { CourierClient } from "@trycourier/courier";
 
 const customerEventBus = {};
 
 // Customer Created Handle
 customerEventBus[CustomerService.Events.CREATED] = async function customerCreatedHandle(
   { logger, customerService }: InjectedDependencies,
-  data: any
+  data: any,
+  client: CourierClient
 ): Promise<EventBusResponse> {
-
-
-  logger.debug("Customer created event, CustomerId:" + data.id);
-  let customer: any;
+  let customer: Customer;
+  let template_data: any = {};
   try {
     customer = (await customerService.retrieve(data.id)) as Customer;
+
+    template_data.content = {
+      title: "Welcome",
+      body: "Welcome to our store",
+    }
+
+    // Send Email
+    await client.send({
+      message: {
+        to: {
+          data: template_data,
+          email: customer.email,
+        },
+        content: template_data.content,
+        routing: {
+          method: "single",
+          channels: ["email"],
+        },
+      },
+    });
+
   } catch (e) {
-    
-    logger.error("notification push failed, CustomerId:" + data.id);
     return {
       to: customer.email,
       status: "failed",
       data: {
         // any data necessary to send the email
         // for example:
-        subject: "welcome to medusa",
+        ...customer
       },
     };
   }
 
-  logger.debug("notification push success, CustomerId:" + data.id);
   return {
     to: customer.email,
     status: "done",
     data: {
       // any data necessary to send the email
       // for example:
-      subject: "welcome to medusa",
+      ...customer
     },
   };
 }
--- src/handle/order.ts
+++ src/handle/order.ts
@@ -1,59 +1,84 @@
-import {
-  OrderService,
-  Order,
-} from "@medusajs/medusa";
+import { OrderService, Order, Customer } from "@medusajs/medusa";
 import { InjectedDependencies, EventBusResponse } from "../types";
+import { CourierClient } from "@trycourier/courier";
 // import SendGrid from "@sendgrid/mail"
-
 
 const orderEventBus = {};
 
-
 // Order Placed Handle
 orderEventBus[OrderService.Events.PLACED] = async (
-  { logger, orderService }: InjectedDependencies,
-  data: any
+  { logger, orderService, customerService }: InjectedDependencies,
+  data: any,
+  client: CourierClient
 ): Promise<EventBusResponse> => {
   logger.debug("Order placed event, OrderId:" + data.id);
 
   let order: Order;
+  let customer: Customer;
+  let template_data: any = {};
   try {
-    order = await orderService.retrieve(data.id);
+    order = await orderService.retrieveWithTotals(data.id);
+    customer = await customerService.retrieve(order.customer_id);
 
-    /** Use SendGrid to send email **/
-    // await SendGrid.send(sendOptions)
-    // .then(() => {
-    //   status = "sent"
-    // })
-    // .catch((error) => {
-    //   status = "failed"
-    //   this.logger_.error(error)
-    // })
+    template_data= {
+      display_id: order.display_id,
+      first_name: customer.first_name,
+      last_name: customer.last_name,
+      updated_at: order.updated_at.toLocaleDateString(),
+      currency_code: order.currency_code,
+      subtotal: (order.subtotal/100).toFixed(2),
+      total: (order.total/100).toFixed(2),
+      items: order.items.map((item) => {
+        return {
+          title: item.title,
+          description: item.description,
+          thumbnail: item.thumbnail,
+          quantity: item.quantity,
+          unit_price: (item.unit_price/100).toFixed(2),
+          original_total: (item.original_total/100).toFixed(2),
+          total: (item.unit_price/100).toFixed(2),
+        }
+      }),
+    }
 
+    template_data.content = {
+      title: "#{{display_id}} Order Placed",
+      body: "Your order has been placed",
+    }
+
+    await client.send({
+      message: {
+        to: {
+          data: template_data,
+          email: order.email,
+        },
+        content: template_data.content,
+        routing: {
+          method: "single",
+          channels: ["email"],
+        },
+      },
+    });
   } catch (e) {
-    logger.error("notification push failed, orderId:" + data.id);
+    logger.error(`notification push failed: ${e.message}, orderId: ${data.id}`);
     return {
       to: order.email,
       status: "failed",
       data: {
-        // any data necessary to send the email
-        // for example:
-        subject: "Good",
+        ...template_data
       },
     };
   }
 
-  logger.debug("notification push success, orderId:" + data.id);
   return {
-    to: order.customer_id,
+    to: order.email,
     status: "done",
     data: {
       // any data necessary to send the email
       // for example:
-      ...order,
-      subject: "Good!",
+      ...template_data,
     },
   };
-}
+};
 
 export default orderEventBus;
